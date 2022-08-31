7ae8aee92288264d2e60c9459ef351b0501db03f
veli-matti heino <veli-matti.heino@metropolia.fi>
2022-08-31 14:38:53 +0300

    jquery exercise 4/5

--- jquery4/img.js
+++ jquery4/img.js
@@ -1,21 +1,21 @@
-$('button').on("click", function() {
+$('button').on('click', function() {
   $('h2:eq(0)').text('select your favourite colour among the colours');
-  $('img:eq(0)').attr("src", "red.jpg");
-  $('img:eq(1)').attr("src", "blue.jpg");
-  $('img:eq(2)').attr("src", "yellow.jpg");
+  $('img:eq(0)').attr('src', "red.jpg");
+  $('img:eq(1)').attr('src', "blue.jpg");
+  $('img:eq(2)').attr('src', "yellow.jpg");
   $('h2:eq(1)').text('your favourite colours in order');
 });
 
-$('img:eq(0)').on("click", function() {
-  $('img:eq(0)').css({'display':'none'});
+$('img:eq(0)').on('click', function() {
+  $('img:eq(0)').css({display:'none'});
   $('ul:eq(0)').append('<li>red</li>');
 });
-$('img:eq(1)').on("click", function() {
-  $('img:eq(1)').css({'display':'none'});
+$('img:eq(1)').on('click', function() {
+  $('img:eq(1)').css({display:'none'});
   $('ul:eq(0)').append('<li>blue</li>');
 });
-$('img:eq(2)').on("click", function() {
-  $('img:eq(2)').css({'display':'none'});
+$('img:eq(2)').on('click', function() {
+  $('img:eq(2)').css({display:'none'});
   $('ul:eq(0)').append('<li>yellow</li>');
 });
 
