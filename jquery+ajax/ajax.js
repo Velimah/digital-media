a658adecd974606c9adccb9a60679378b7fa3558
veli-matti heino <veli-matti.heino@metropolia.fi>
2022-08-31 16:32:58 +0300

    jquery exercise ajax

---
+++ jquery+ajax/ajax.js
@@ -0,0 +1,23 @@
+$('#hakulomake').submit(function(evt) {
+  evt.preventDefault();
+
+  const osoite = $(this).attr('action');
+  const hakulause = $(this).serialize();
+  const url = osoite + hakulause;
+
+  $.getJSON(url, function(sarjat){
+    console.log(sarjat);
+    $.each(sarjat, function(i, sarja) {
+      console.log(sarja.show.name);
+      const nimi = sarja.show.name;
+      const kotisivu = sarja.show.officialSite || sarja.show.url;
+      const kuva = sarja.show.image.medium;
+      const yhteenveto = sarja.show.summary;
+      $('#tulos').append(
+          '<h2>'+nimi+'</h2>'+
+          '<img src="'+kuva+'" alt="title">'+
+          '<p>'+yhteenveto+'</p>'+
+          '<p><a href="'+kotisivu+'">kotisivu</a></p>');
+    });
+  });
+});
